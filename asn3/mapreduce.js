24f51af4bcb0def44a35e7e8345cf6b7cd8301c9
Saud Rehman <saudrehman00@hotmail.com>
2023-04-09 17:42:23 -0400

    Asn 3 completed

---
+++ asn3/mapreduce.js
@@ -0,0 +1,13 @@
+function myMapper() {
+    this.entities.hashtags.forEach(function (hashtag){
+        emit(hashtag, 1);
+    })
+}
+
+function myReducer(key, values) {
+    return Array.sum(values);
+}
+
+db.tweets.mapReduce(myMapper, myReducer, { out: "mroutput" });
+
+db.mroutput.find().sort({ value: -1 });
